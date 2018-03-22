833ad39fdde02e22304ccaa4e443d7f95ce8ddb9
Srinivas <sri@vulab.com>
2018-03-21 19:23:08 -0500

    Changed added a space

--- VulabRestWebService/src/main/java/com/vulab/VulabRestWebService/repositories/EmployeeRepository.java
+++ VulabRestWebService/src/main/java/com/vulab/VulabRestWebService/repositories/EmployeeRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.vulab.VulabRestWebService.domain.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
-	
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{	
 
 }
